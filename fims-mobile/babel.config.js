457c37c31433313b7ae943a7d907e0eec549eb64
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-11-14 23:45:32 +0530

    Added COMPILATION_ERROR_FIX.md

--- fims-mobile/babel.config.js
+++ fims-mobile/babel.config.js
@@ -2,11 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
   };
 };
